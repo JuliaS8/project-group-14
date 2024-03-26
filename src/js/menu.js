751b8f590f8c17f198df0a2a1aefcfb3a69bbc34
Oleksandr Boiko <boiko20009@gmail.com>
2024-03-26 18:40:44 +0200

    добавил скрипты на анимацию

--- src/js/menu.js
+++ src/js/menu.js
@@ -12,22 +12,65 @@
 //     }
 //   });
 // });
+// (() => {
+//   const refs = {
+//     // Додати атрибут data-modal-open на кнопку відкриття
+//     openMenuBtn: document.querySelector("[data-menu-open]"),
+//     // Додати атрибут data-modal-close на кнопку закриття
+//     closeMenuBtn: document.querySelector("[data-menu-close]"),
+//     // Додати атрибут data-modal на бекдроп модалки
+//     menu: document.querySelector("[data-menu]"),
+//   };
+
+//   refs.openMenuBtn.addEventListener("click", toggleMenu);
+//   refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+//   function toggleMenu() {
+//     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+//     refs.menu.classList.toggle("is-open");
+//   }
+  
+// })();
 (() => {
-  const refs = {
-    // Додати атрибут data-modal-open на кнопку відкриття
-    openMenuBtn: document.querySelector("[data-menu-open]"),
-    // Додати атрибут data-modal-close на кнопку закриття
-    closeMenuBtn: document.querySelector("[data-menu-close]"),
-    // Додати атрибут data-modal на бекдроп модалки
-    menu: document.querySelector("[data-menu]"),
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+
+  const toggleMenu = () => {
+    const anchors = mobileMenu.querySelectorAll('a[href*="#"]');
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+    
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+    
+    if(anchors.length === 0) return;
+    
+    if(!isMenuOpen) {
+      anchors.forEach(anchor => {
+        anchor.addEventListener("click", toggleMenu)
+      })
+      return;
+    }
+    
+    anchors.forEach(anchor => {
+        anchor.removeEventListener("click", toggleMenu)
+      })
   };
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
 
-  function toggleMenu() {
-    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-    refs.menu.classList.toggle("is-open");
-  }
-  
+  // Вказати брейкпоінт після якого повинна зачинятися
+  window.matchMedia('(min-width: 375px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
 })();
