a3e6abe8f394639fc54388015495da13acdc242b
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-03-27 02:11:27 +0100

    reviews file

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,21 +1,46 @@
-const swiper = new Swiper('.reviews-swiper', {
-  // Optional parameters
-  direction: 'vertical',
+new Swiper('.reviews-swiper', {
+  // кількість слайдів на сторінці
+  slidesPerView: 1,
+
   loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
+  // відступ між слайдами
+  spaceBetween: 16,
+
+  autoplay: {
+    delay: 3000,
   },
 
-  // Navigation arrows
+  pagination: {
+    el: '.swiper-pagination',
+    // type: 'fraction',
+
+    type: 'custom',
+    renderCustom: function (swiper, current, total) {
+      return `<span class="current">${String(current).padStart(
+        2,
+        '0'
+      )}</span><span class="delimiter"></span><span class="total">${String(
+        total
+      ).padStart(2, '0')}</span>`;
+    },
+
+    // type: 'bullets',
+    // clickable: true,
+  },
+
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
+  // Брейк-поінти
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+    },
   },
 });
